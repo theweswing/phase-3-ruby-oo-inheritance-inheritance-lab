114afde47ada76329e63f5f86f2ddeba11b246d2
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-11-28 17:17:09 -0500

    completed

---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+class User
+  def first_name
+    @first_name
+  end
+
+  def first_name=(name)
+    @first_name = name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def last_name=(name)
+    @last_name = name
+  end
+end
